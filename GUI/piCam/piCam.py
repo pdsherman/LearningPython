3001f81a7bc9ad0d7b313f53721f1d06367bbe6c
pdsherman <pdsherman201@gmail.com>
2016-05-13 09:03:44 -0700

    Created first parts of GUI for program

--- GUI/piCam/piCam.py
+++ GUI/piCam/piCam.py
@@ -9,16 +9,31 @@ dynamically interact with camera as well as platform to
 learn computer vision techniques.
 """
 from Tkinter import *
-from quitter import Quitter
+from tkMessageBox import askokcancel
+from os import sys
+
+def quit():
+	ans = askokcancel("Verify exit", "Really quit?")
+	if ans:
+		sys.exit(0)
 
 #Create frame
 root = Tk()
 root.title('Computer Vision Playground')
 
+#Add Quit button
+row = Frame(root)
+row.config(width=500)
+btnQuit = Button(row, text="Quit", command=quit)
+btnTest = Button(row, text="Test12")
+btnQuit.pack(side=RIGHT)
+btnTest.pack(side=RIGHT)
+row.pack(side=BOTTOM)
 
-
-Quitter(root).pack(fill = X)
-
+#Add Canvas for showing images
+cnvs = Canvas(root)
+cnvs.pack(fill=BOTH, side=TOP)
+cnvs.config(width=100, height=100)
 
 #---------------#
 #  Main Loop 
