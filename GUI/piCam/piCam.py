d6ac7adebcafed035d290a9ec35c9d80774b4d15
pdsherman <patrick@momentummachines.com>
2016-05-13 18:18:01 -0700

    More progress on GUI for PiCam. Now have Quit and Open File buttons on bottom Canvas on top can display an image. Pressing Open File button will open filesystem for user to select image file to display in GUI Also started dividing buttons and canvas into separate classes. Should consider creating a single class for the overall GUI that contains instances of the image canvas and button bar.

--- GUI/piCam/piCam.py
+++ GUI/piCam/piCam.py
@@ -1,7 +1,7 @@
 """
 File:   piCam.py
 Author: pdsherman
-Date:   11 May 2016
+Date:   May 2016
 
 Description: Program is meant to be run on raspberry pi with 
 camera connected. Goal is to create GUI that can user can
@@ -10,32 +10,99 @@ learn computer vision techniques.
 """
 from Tkinter import *
 from tkMessageBox import askokcancel
-from os import sys
+from tkFileDialog import askopenfilename
+from PIL import Image
+from PIL.ImageTk import PhotoImage
+from os import sys, path
 
-def quit():
-	ans = askokcancel("Verify exit", "Really quit?")
-	if ans:
-		sys.exit(0)
+class ButtonBar(Frame):
+    """
+    Created a bar of buttons at the bottom of
+    the main window. Buttons include following:
+    Quit:       End the program
+    Open File:  User picks file to open and display in window. 
+    """
+    def __init__(self, parent=None, cnvImg=None, **options):
+        Frame.__init__(self, parent, **options)
+       
+        self.cnvImg = cnvImg
+
+        btnQuit = Button(self, text="Quit", command=self.quit)
+        btnFile = Button(self, text="Open File", command=self.getFile)
+        
+        self.btns = []
+        self.btns.append(btnQuit) 
+        self.btns.append(btnFile) 
+
+        for btn in self.btns:
+            btn.config(width=10, height=2)
+            btn.pack(side=RIGHT)
+      
+        self.config(bd=3, relief=RIDGE)
+        self.pack(side=BOTTOM, fill=X)
+
+    def quit(self):
+        """
+        Brings up dialog box to ask user if they
+        want to quit program
+        """
+        if askokcancel("Verify Exit", "Really quit?"):
+            sys.exit(0)
+
+    def getFile(self):
+        """
+        Open dialog box for user to select image from
+        file system to display
+        """
+        if(self.cnvImg != None):
+            cnvImg.createImg(askopenfilename())
+    
+    def setCanvasImage(self, cnvImg):
+        """
+        Set object canvas 
+        """
+        self.cnvImg = cnvImg
+
+class CanvasImage(Canvas):
+    """
+    Canvas object to hold and display object
+    """
+    def __init__(self, parent=None, width=350, height=300, imgFile=None, **options):
+        Canvas.__init__(self, parent, **options)
+        
+        #Setup for Canvas on GUI window
+        self.config(width=width, height=height, bd=3, relief=RIDGE)
+        self.pack(fill=BOTH, expand=True, side=TOP)
+       
+        #Image file name stored on canvas.
+        self.createImg(imgFile)
+
+    def createImg(self, filename=None):
+        if(filename != None):
+            self.setImageFile(filename)
+        
+        if(self.imgFile != None):
+            self.img = PhotoImage(file=self.imgFile)
+            self.config(width=self.img.width(), height=self.img.height())
+            self.create_image(2, 2, image=self.img, anchor=NW)
+
+    def setImageFile(self, filename):
+        #Check if valid file 
+        #TODO: check if valid image file
+        if(filename != None and path.isfile(filename)):
+            self.imgFile = filename
+        else:
+            self.imgFile = None
 
 #Create frame
 root = Tk()
 root.title('Computer Vision Playground')
 
-#Add Quit button
-row = Frame(root)
-row.config(width=500)
-btnQuit = Button(row, text="Quit", command=quit)
-btnTest = Button(row, text="Test12")
-btnQuit.pack(side=RIGHT)
-btnTest.pack(side=RIGHT)
-row.pack(side=BOTTOM)
-
-#Add Canvas for showing images
-cnvs = Canvas(root)
-cnvs.pack(fill=BOTH, side=TOP)
-cnvs.config(width=100, height=100)
+btnBar = ButtonBar(root)
+cnvImg = CanvasImage(root, imgFile="./atlas.png")
+btnBar.setCanvasImage(cnvImg)
 
 #---------------#
-#  Main Loop 
+#   Main Loop   #
 #---------------#
 root.mainloop()
