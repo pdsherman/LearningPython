9157ea02b17cd204272bde79f7c3e350a32f4c42
pdsherman <pdsherman201@gmail.com>
2016-05-14 23:42:00 -0700

    Added new class for main GUI instance. Class holds button bar and image canvas object. Displaying image now uses PIL library to scale any image to the same size. ButtonBar class takes in a dictionary of button text and corresponding commands.

--- GUI/piCam/piCam.py
+++ GUI/piCam/piCam.py
@@ -16,93 +16,96 @@ from PIL.ImageTk import PhotoImage
 from os import sys, path
 
 class ButtonBar(Frame):
-    """
-    Created a bar of buttons at the bottom of
-    the main window. Buttons include following:
-    Quit:       End the program
-    Open File:  User picks file to open and display in window. 
-    """
-    def __init__(self, parent=None, cnvImg=None, **options):
-        Frame.__init__(self, parent, **options)
-       
-        self.cnvImg = cnvImg
-
-        btnQuit = Button(self, text="Quit", command=self.quit)
-        btnFile = Button(self, text="Open File", command=self.getFile)
-        
-        self.btns = []
-        self.btns.append(btnQuit) 
-        self.btns.append(btnFile) 
-
-        for btn in self.btns:
-            btn.config(width=10, height=2)
-            btn.pack(side=RIGHT)
+	"""
+	Created a bar of buttons at the bottom of
+   the main window.
+	"""
+	def __init__(self, parent=None, buttons=None, **options):
+		Frame.__init__(self, parent, **options)
       
-        self.config(bd=3, relief=RIDGE)
-        self.pack(side=BOTTOM, fill=X)
+   	#List of all buttons in bar 
+		self.btns = []
 
-    def quit(self):
-        """
-        Brings up dialog box to ask user if they
-        want to quit program
-        """
-        if askokcancel("Verify Exit", "Really quit?"):
-            sys.exit(0)
-
-    def getFile(self):
-        """
-        Open dialog box for user to select image from
-        file system to display
-        """
-        if(self.cnvImg != None):
-            cnvImg.createImg(askopenfilename())
-    
-    def setCanvasImage(self, cnvImg):
-        """
-        Set object canvas 
-        """
-        self.cnvImg = cnvImg
-
-class CanvasImage(Canvas):
-    """
-    Canvas object to hold and display object
-    """
-    def __init__(self, parent=None, width=350, height=300, imgFile=None, **options):
-        Canvas.__init__(self, parent, **options)
+		#Loop through "buttons" to create all buttons in bar 
+		for name, func in buttons.iteritems():
+			btn = Button(self, text=name, command=func, width=10, height=2)	
+			btn.pack(side=RIGHT)
+     		self.btns.append(btn) 
+	
+		#Config and pack frame for button bar	
+		self.config(bd=3, relief=RIDGE)
+		self.pack(side=BOTTOM, fill=X)
+  
+class ImageCanvas(Canvas):
+	"""
+	Canvas object to hold and display object
+	"""
+	def __init__(self, parent=None, width=350, height=300, imgFile=None, **options):
+		Canvas.__init__(self, parent, **options)
         
-        #Setup for Canvas on GUI window
-        self.config(width=width, height=height, bd=3, relief=RIDGE)
-        self.pack(fill=BOTH, expand=True, side=TOP)
+		#Setup for Canvas on GUI window
+		self.config(width=width, height=height, bd=3, relief=RIDGE)
+		self.pack(fill=BOTH, expand=True, side=TOP)
        
-        #Image file name stored on canvas.
-        self.createImg(imgFile)
+		#Image file name stored on canvas.
+		self.createImg(imgFile)
 
-    def createImg(self, filename=None):
-        if(filename != None):
-            self.setImageFile(filename)
+	def createImg(self, filename=None):
+		if(filename != None):
+			self.setImageFile(filename)
         
-        if(self.imgFile != None):
-            self.img = PhotoImage(file=self.imgFile)
-            self.config(width=self.img.width(), height=self.img.height())
-            self.create_image(2, 2, image=self.img, anchor=NW)
+		if(self.imgFile != None):
+			#try:	
+			self.imgObj = Image.open(self.imgFile)
+			self.imgObj.thumbnail((350, 300), Image.ANTIALIAS)	
+			self.photo = PhotoImage(self.imgObj)
+		
+			#self.config(width=self.img.width(), height=self.img.height())
+			self.create_image(175, 150, image=self.photo, anchor=CENTER)
+			#except:
+			#File wasn't valid image
+			self.imgFile = None
 
-    def setImageFile(self, filename):
-        #Check if valid file 
-        #TODO: check if valid image file
-        if(filename != None and path.isfile(filename)):
-            self.imgFile = filename
-        else:
-            self.imgFile = None
+	def setImageFile(self, filename):
+		#Check if valid file 
+		#TODO: ?check if valid image file?
+		if(filename != None and path.isfile(filename)):
+			self.imgFile = filename
+		else:
+			self.imgFile = None
 
-#Create frame
-root = Tk()
-root.title('Computer Vision Playground')
+class PiCamGUI():
+	"""
+	Object to create and display the GUI for the PiCam
+	program. Window contains button bar for user options
+	and image canvas to display images
+	"""
+	def __init__(self, parent=None, **options):
+		#Root of GUI	
+		self.root = Tk()
+		self.root.title("Computer Vision Playground")
 
-btnBar = ButtonBar(root)
-cnvImg = CanvasImage(root, imgFile="./atlas.png")
-btnBar.setCanvasImage(cnvImg)
+		#Dictionary of texts and functions for button bar
+		self.buttons = {"Quit": self.quit, "Open File": self.fileCmd}
 
-#---------------#
-#   Main Loop   #
-#---------------#
-root.mainloop()
+		#Populate GUI with button bar and canvas image
+		self.btnBar = ButtonBar(self.root, self.buttons)
+		self.cnvImg = ImageCanvas(self.root, imgFile="./atlas.png")
+	
+	def mainloop(self):
+		""" Run program """
+		self.root.mainloop()
+
+	def fileCmd(self):
+		""" Update image file of ImageCanvas """
+		self.cnvImg.createImg(askopenfilename())
+
+	def quit(self):
+		""" Brings up dialog box to ask user if they
+		want to quit program """
+		if askokcancel("Verify Exit", "Really quit?"):
+			sys.exit(0)
+
+if __name__ == "__main__":
+	gui = PiCamGUI()
+	gui.mainloop()
